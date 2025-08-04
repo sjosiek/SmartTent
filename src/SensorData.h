a59fc751a5e84f6b0ce5bc88e65ea5a4d835da70
Sebastian Josiek <sebastian.josiek@soflab.pl>
2025-08-04 12:25:03 +0200

    Poprawki kosmetyczne w zmiennych

---
+++ src/SensorData.h
@@ -0,0 +1,18 @@
+#ifndef SENSORDATA_H
+#define SENSORDATA_H
+
+#include <Arduino.h>
+
+struct SensorData {
+  String dateStr;
+  String timeForLcd;
+  String timeForLed;
+  float temp_bme;
+  float hum_bme;
+  float pressure_bme;
+  float temp_rtc;
+  float temp_dht;
+  float hum_dht;
+};
+
+#endif
